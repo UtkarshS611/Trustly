d376cc59870afb98cb0345f7e4295476f8148541
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2025-01-09 18:04:23 +0530

    authentication complete

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,23 +1,22 @@
 "use client"
 
+// import Image from 'next/image';
+
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const Hero = () => {
 
-    const [element, setElement] = React.useState(false);
+    const router = useRouter();
 
-  const handleClick = () => {
-    setElement(true);
-
-    setTimeout(() => {
-        setElement(false);
-      }, 2000); 
-  };
+    const handleclick = () => {
+        router.push('/sign-in')
+    }
 
   return (
     <section className='pt-20'>
-        <div className={`absolute text-red-600 text-center duration-100 bg-red-400/10 overflow-hidden left-[50%] translate-x-[-50%] top-[10vh] rounded-[8px] px-4 py-1 ${element? "h-[2rem] w-[11.9rem] opacity-100" : "w-0 h-0 opacity-0"}`}>Someting went wrong
-        </div>
+        {/* <div className={`absolute text-red-600 text-center duration-100 bg-red-400/10 overflow-hidden left-[50%] translate-x-[-50%] top-[10vh] rounded-[8px] px-4 py-1`}>Someting went wrong
+        </div> */}
         <div className='uppercase flex flex-col text-center xl:text-5xl lg:text-4xl md:text-3xl text-2xl'>
             <span className='text-center leading-relaxed'>Achieve your dream</span>
             <span className='text-center'>job safely and effortlessly</span>
@@ -27,10 +26,10 @@ const Hero = () => {
             <span>We make it easier for you to check job post authenticity while you focus on your skills.</span>
         </div>
         <div className='flex justify-center mt-8'>
-            <input type="text" placeholder='Username / Email' className='outline-none rounded-l-full bg-white/10 px-4 py-2'/>
-            <button onClick={handleClick} className='bg-white/80 text-black rounded-r-full px-4 py-2'>Get started</button>
+            <button onClick={handleclick} className='bg-white/10 shadow py-2 px-4 rounded-full flex items-center gap-1 cursor-pointer'>Get started</button>
         </div>
-        <div className='w-[80%] h-[50vh] md:h-[60vh] lg:h-[80vh] border-[5px] border-[#3BD6FF] border-opacity-70 mx-auto rounded-2xl mt-20'>
+        <div className='w-[80%] h-[50vh] md:h-[60vh] lg:h-[80vh] border-[5px] border-[#3BD6FF] border-opacity-70 mx-auto rounded-2xl overflow-hidden mt-20 bg-[url("/car.jpg")] bg-no-repeat bg-cover'>
+            
         </div>
     </section>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,14 @@ import Hero from './components/Hero'
 import Faqs from './components/Faqs'
 import Footer from './components/Footer'
 import Features from './components/Features'
+import Gradient from './components/Gradient'
+import ScrollToTop from './components/ScrollToTop'
 
 const Home = () => {
   return (
     <div className='text-white/85'>
+      <ScrollToTop/>
+      <Gradient/>
       <main>
         <Navbar/>
         <Hero/>
